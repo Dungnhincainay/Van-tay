2f2a1d397ba1a38d7e1ccc82541796c8278c4e89
Dungnhincainay <dungnhincainay@gmail.com>
2021-03-29 00:25:36 +0700

    test

--- Van_Tay_Hien_thi/Van_Tay_Hien_thi.ino
+++ Van_Tay_Hien_thi/Van_Tay_Hien_thi.ino
@@ -4,42 +4,34 @@
 #include <Adafruit_Fingerprint.h>
 #include <Wire.h> 
 #include <LiquidCrystal_I2C.h>
-LiquidCrystal_I2C lcd(0x27,16,2); 
-int button = 5;                   // Nối chân "touch" cảm biến
 
+LiquidCrystal_I2C lcd(0x27,16,2); 
 SoftwareSerial mySerial(2, 3);    // Day tim chan D2, Day xanh chan D3
 Adafruit_Fingerprint finger = Adafruit_Fingerprint(&mySerial);
 
-//int button = 5;
-void setup()  
-{
-  Serial.begin(9600);
-  pinMode(5, INPUT); 
+//int button = 5;                   // Nối chân "touch" cảm biến
+int Van_tay;
+#define INTERRUPT_PIN 5
 
-  pinMode(9, OUTPUT);
-  pinMode(10, OUTPUT);
-  pinMode(11, OUTPUT);
-  pinMode(12, OUTPUT);
+int led = 6;
+int flag= 0;
+int ledState = LOW;
+unsigned long Ago = 0;
+unsigned long Now = 0;
+const long Set_time = 300; // Thời gian đèn led nhấp nháy
+
+void ICACHE_RAM_ATTR Tim_Van_Tay(){
   
-  lcd.init();              // Xoá lcd     
-  lcd.backlight();
-  while (!Serial);        // For Yun/Leo/Micro/Zero/...
-  delay(100);
-  Serial.println("\n\n Please Wait....\n");
+  digitalWrite(12, HIGH);   // Đèn led sáng
+  
+}
 
-  finger.begin(57600);
-  delay(15);
-  if (finger.verifyPassword()) {
-    Serial.println("Find done");
-  } else {
-    Serial.println("Sensor not Connected :(");
-    lcd.setCursor(0,0);
-    lcd.print("Sensor Not Connected...");
-    while (1) { delay(1); }
-  }
+void Start_Up(){
 
+  
   finger.getTemplateCount();
-  Serial.print("Cam bien chua "); Serial.print(finger.templateCount); 
+  Serial.print("Cam bien chua "); 
+  Serial.print(finger.templateCount); 
   Serial.println(" mau van tay.");
   Serial.println("Dang doi mau van tay hop le ...");
   
@@ -55,14 +47,43 @@ void setup()
   lcd.print("Cam bien......");
 }
 
-void xanh_on(){
-  digitalWrite(11, HIGH);
-  delay(200);
+void setup()  
+{
+  Serial.begin(9600);
+  pinMode(5, INPUT); 
+
+  pinMode(6, OUTPUT);     // Xuất tín hiệu
+  pinMode(9, OUTPUT);     // Xuất tín hiệu
+  pinMode(10, OUTPUT);    // Xuất tín hiệu
+  pinMode(11, OUTPUT);    // Xuất tín hiệu
+  pinMode(12, OUTPUT);    // real time
+  pinMode(13, OUTPUT);    // Touch
+  
+  
+  //pinMode(INTERRUPT_PIN, INPUT_PULLUP);                   // CHÚ Ý 2 DÒNG NÀY_____Khai báo Ngắt
+  //attachInterrupt(INTERRUPT_PIN, Tim_Van_Tay, FALLING);  // CHÚ Ý 2 DÒNG NÀY_____RISING- có 5 chế độ ngắt
+
+  lcd.init();              // Xoá lcd     
+  lcd.backlight();
+  while (!Serial);        // For Yun/Leo/Micro/Zero/...
+  delay(100);
+  Serial.println("\n\n Please Wait....\n");
+  finger.begin(57600);
+  delay(15);
+
+  if (finger.verifyPassword()) {
+    lcd.setCursor(3,0);
+    lcd.print("Find done !");
+    Serial.println("Find done");
+  } else {
+    Serial.println("Sensor not Connected :(");
+    lcd.setCursor(0,0);
+    lcd.print("Sensor Not Connected...");
+    while (1) { delay(1); }
+  }
+  Start_Up();
 }
-void xanh_off(){
-  digitalWrite(11, LOW);
-  delay(200);
-}
+
 void do_on(){
   digitalWrite(9,HIGH);
   delay(200);
@@ -71,26 +92,72 @@ void do_off(){
   digitalWrite(9,LOW);
   delay(200);
 }
+void xanh_on(){
+  digitalWrite(11, HIGH);
+  delay(200);
+}
+void xanh_off(){
+  digitalWrite(11, LOW);
+  delay(200);
+}
  
 void nhap_nhay()
 {
-    for (byte i = 0; i < 3; i++) {
-        digitalWrite(12, HIGH);
+    for (byte i = 1; i < 3; i++) {  // Nhap nhay 2 lan
+        digitalWrite(13, HIGH);
         delay(200);
-        digitalWrite(12 , LOW);
+        digitalWrite(13, LOW);
         delay(200);
     }
 }
 
 void loop()            // run over and over again
 {
-  int buttonStatus = digitalRead(5);    //Đọc trạng thái button
-  if (buttonStatus == HIGH) {         // Nếu mà button bị nhấn
-    digitalWrite(12, HIGH);
-                     // Đèn led sáng
-    }
+  //int buttonStatus = digitalRead(5);    //Đọc trạng thái button
+  if (digitalRead(5)){  // Phát hiện chạm
+    nhap_nhay();
+  }
+
+  Now = millis();                 // Nhấp nháy led
+  if (Now - Ago >= Set_time) {    // Nhấp nháy led
+    Ago = Now;                    // Nhấp nháy led
+    if (ledState == LOW) {        // Nhấp nháy led
+      ledState = HIGH;            // Nhấp nháy led
+    } else {                      // Nhấp nháy led
+      ledState = LOW;             // Nhấp nháy led
+    }                             // Nhấp nháy led
+    digitalWrite(led, ledState);      
+
   getFingerprintIDez();
   delay(50);            //don't ned to run this at full speed.
+  
+  if (Van_tay== -1){      //  Tắt tất cả
+    digitalWrite(9,LOW);
+    digitalWrite(10,LOW);
+    digitalWrite(11,LOW);
+
+    digitalWrite(12, HIGH);
+    delay(500);
+    digitalWrite(12,LOW);
+  }
+  if (Van_tay>5){
+    if((Van_tay/10)==1){  // Mở khoá
+      digitalWrite(11, HIGH);
+      delay(500);
+      digitalWrite(11,LOW);
+    }else{
+      if((Van_tay/10)==2){  // Mở Cốp
+      digitalWrite(9,HIGH);
+      delay(500);
+      digitalWrite(9,LOW);
+      }else{
+        digitalWrite(10,HIGH);
+        delay(500);
+        digitalWrite(10,LOW);
+      }
+    }
+  }
+  
 }
 
 uint8_t getFingerprintID() {
@@ -111,9 +178,7 @@ uint8_t getFingerprintID() {
     default:
       Serial.println("Lỗi không xác định");
       return p;
-  }
-
-  // OK success!
+  }  // OK success!
 
   p = finger.image2Tz();
   switch (p) {
@@ -135,9 +200,8 @@ uint8_t getFingerprintID() {
     default:
       Serial.println("Lỗi không xác định :( ");
       return p;
-  }
-  
-  // OK converted!
+  }  // OK converted!
+
   p = finger.fingerFastSearch();
   if (p == FINGERPRINT_OK) {
     Serial.println("Tìm thấy một dấu vân tay phù hợp!");
@@ -145,14 +209,15 @@ uint8_t getFingerprintID() {
     Serial.println("Lỗi giao tiếp");
     return p;
   } else if (p == FINGERPRINT_NOTFOUND) {
+    Van_tay = -1;
     Serial.println("Không tìm thấy dấu vân tay phù hợp");
     return p;
   } else {
     Serial.println("Lỗi không xác định :( ");
     return p;
-  }   
+  }    // found a match!
   
-  // found a match!
+  Van_tay = finger.fingerID;
   Serial.print("ID Tìm thấy #"); Serial.print(finger.fingerID); 
   Serial.print(" với độ tin cậy là "); Serial.println(finger.confidence); 
 
@@ -171,6 +236,7 @@ int getFingerprintIDez() {
   if (p != FINGERPRINT_OK)  return -1;
   
   // found a match!
+  Van_tay = finger.fingerID;
   Serial.print("ID Tìm thấy #"); Serial.print(finger.fingerID); 
   Serial.print(" với độ tin cậy là "); Serial.println(finger.confidence);
 
@@ -182,25 +248,7 @@ int getFingerprintIDez() {
   lcd.setCursor(5,1);
   lcd.print(finger.fingerID);
 
-  int id= finger.fingerID;
-  if((id/10)==1){
-    digitalWrite(11, HIGH);
-    delay(500);
-    digitalWrite(11,LOW);
-  }else{
-    if((id/10)==2){
-    digitalWrite(9,HIGH);
-    delay(500);
-    digitalWrite(9,LOW);
-    }else{
-        digitalWrite(10,HIGH);
-        delay(500);
-        digitalWrite(10,LOW);
-      }
-      
-  }
   return finger.fingerID; 
   delay(200);
   digitalWrite(12 , LOW);
-  delay(200);
 }
